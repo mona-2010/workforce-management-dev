57a285d964d0163d5c5db9ebfc4893d134d812cb
unknown <monapadhy02@gmail.com>
2026-04-21 16:19:34 +0530

    test: dfghjkloiuyugvbnji

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 
 export default function Home() {
-  const name = 'Monalisa';
+  const name = 'Mona';
   return (
     <div>
-      <h1>HEllo, Monalisaaa </h1>
+      <h1 className="text-blue-400">Hello, Monaaaa </h1>
       <p>{name}</p>
     </div>
   );
